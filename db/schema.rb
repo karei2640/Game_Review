9ec2f53c6be3e797ea2da724cc39f6e5d24a02ff
EC2 Default User <ec2-user@ip-172-31-18-22.ap-northeast-1.compute.internal>
2023-04-02 03:22:01 +0000

    マイグレーションの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_02_030249) do
+ActiveRecord::Schema.define(version: 2023_04_02_032115) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2023_04_02_030249) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "customer_id", null: false
+    t.integer "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_comments_on_customer_id"
+    t.index ["game_id"], name: "index_comments_on_game_id"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -42,6 +52,15 @@ ActiveRecord::Schema.define(version: 2023_04_02_030249) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_favorites_on_customer_id"
+    t.index ["game_id"], name: "index_favorites_on_game_id"
+  end
+
   create_table "games", force: :cascade do |t|
     t.string "genre_id", null: false
     t.string "game_name", null: false
@@ -58,4 +77,8 @@ ActiveRecord::Schema.define(version: 2023_04_02_030249) do
     t.integer "genre_id"
   end
 
+  add_foreign_key "comments", "customers"
+  add_foreign_key "comments", "games"
+  add_foreign_key "favorites", "customers"
+  add_foreign_key "favorites", "games"
 end
