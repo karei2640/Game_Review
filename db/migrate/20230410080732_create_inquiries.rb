d03a27dce90b02794f6fe8ea1289967cc480b7ff
EC2 Default User <ec2-user@ip-172-31-18-22.ap-northeast-1.compute.internal>
2023-04-15 08:12:02 +0000

    add

--- db/migrate/20230410080732_create_inquiries.rb
+++ db/migrate/20230410080732_create_inquiries.rb
@@ -1,7 +1,7 @@
 class CreateInquiries < ActiveRecord::Migration[6.1]
   def change
     create_table :inquiries do |t|
-      t.string "category", null: false
+      t.string :status
       t.string "name", null: false
       t.string "email", null: false
       t.text "message", null: false
