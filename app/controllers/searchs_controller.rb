e6137374bb70fa2f202de9496f6d190f79d6ea0e
EC2 Default User <ec2-user@ip-172-31-18-22.ap-northeast-1.compute.internal>
2023-04-25 02:39:16 +0000

    エラーメッセージの修正、検索機能の調整

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,8 +1,27 @@
 class SearchsController < ApplicationController
   def index
     @keyword = params[:keyword]
-    @games = Game.where("game_title LIKE ?", "%#{params[:keyword]}%").order(view_count: :desc).page(params[:page]).per(15)
-    @bordgames = Bordgame.where("game_title LIKE ?", "%#{params[:keyword]}%").order(bord_view_count: :desc).page(params[:page]).per(15)
-    @customers = Customer.where("name LIKE ?", "%#{params[:keyword]}%").page(params[:page]).per(15)
+    @genre_id = params[:genre_id]
+    if @genre_id.present? && @keyword.present?
+    @games = Game.where(genre_id: @genre_id).where("game_title LIKE ?", "%#{@keyword}%").order(view_count: :desc).page(params[:page]).per(15)
+    elsif @genre_id.present?
+      @games = Game.where(genre_id: @genre_id).order(view_count: :desc).page(params[:page]).per(15)
+    elsif @keyword.present?
+      @games = Game.where("game_title LIKE ?", "%#{@keyword}%").order(view_count: :desc).page(params[:page]).per(15)
+    else
+      @games = Game.none
+    end
+      
+    @table_id = params[:table_id]
+    if @table_id.present? && @keyword.present?
+    @bordgames = Bordgame.where(table_id: @table_id).where("game_title LIKE ?", "%#{@keyword}%").order(bord_view_count: :desc).page(params[:page]).per(15)
+    elsif @table_id.present?
+      @bordgames = Bordgame.where(table_id: @table_id).order(bord_view_count: :desc).page(params[:page]).per(15)
+    elsif @keyword.present?
+      @bordgames = Bordgame.where("game_title LIKE ?", "%#{@keyword}%").order(bord_view_count: :desc).page(params[:page]).per(15)
+    else
+      @bordgames = Bordgame.none
+    end
+    @customers = Customer.where("name LIKE ?", "%#{@keyword}%").page(params[:page]).per(15)
   end
 end
